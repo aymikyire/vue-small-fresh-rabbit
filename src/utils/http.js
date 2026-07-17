2c726ead79efba705cce081086c48374cc668fee
lyxin <3077250484@qq.com>
2026-07-17 15:22:45 +0800

    home-新鲜好物业务实现(利用HomePannel的props以及插槽slot实现）

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 
 const httpInstance = axios.create({
-    baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout:5000
+    baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout:10000
 
 })
 
