7b71a7904c78f2db3ff91faeb17586f8bfbc06c1
lyxin <3077250484@qq.com>
2026-07-21 11:04:43 +0800

    一级分类-激活状态显示,分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -44,6 +44,26 @@ onMounted(()=>getBanner())
       </el-carousel-item>
     </el-carousel>
   </div>
+  <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink :to="`/category/sub/${i.id}`"> <!-- 点击跳转到二级路由页面 -->
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
+    
     </div>
   </div>
 </template>
