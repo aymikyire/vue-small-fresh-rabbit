fe19e4619512962aa081efc1211d61ada104d77a
lyxin <3077250484@qq.com>
2026-07-22 17:19:18 +0800

    图片预览组件-props适配以及优化操作

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -78,7 +78,7 @@ watch([elementX,elementY], () => {
     <!-- 放大镜大图 -->
     <div class="large" v-show="!isOutside" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
         backgroundPositionX: `${positionX}px`,
         backgroundPositionY: `${positionY}px`,
       },
