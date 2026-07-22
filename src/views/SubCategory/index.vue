168378b0f3f1a4b179fab1e128e5baa5e245ae04
lyxin <3077250484@qq.com>
2026-07-22 09:43:47 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -6,6 +6,7 @@ import { onMounted,ref } from 'vue';
 import { useRoute } from "vue-router"
 import GoodsItem from '../Home/components/GoodsItem.vue';
 
+
 // //获取面包屑导航数据
 // const id = Number(useRoute().params.id)//从路由' path '中提取的解码参数的对象
 // const store=useCatgorySubCategory()
@@ -37,6 +38,28 @@ const getGoodList = async () => {
   goodList.value = res.result.items
 }
 onMounted(() => getGoodList())
+
+//tab切换回调
+const tabChange = () => {
+  console.log('tab切换了',reqData.value.sortField)
+  reqData.value.page=1
+  getGoodList()
+}
+
+//加载更多
+const disabled = ref(false)
+const load = async() => {
+  console.log('加载更多数据咯')
+  //获取下一页的数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value=[...goodList.value,...res.result.items]//展开运算符
+  //加载完毕 停止监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
+
+}
 </script>
 
 <template>
@@ -50,12 +73,12 @@ onMounted(() => getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load"  :infinite-scroll-disabled="disabled">
         <!-- 商品列表 -->
         <GoodsItem v-for ="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
