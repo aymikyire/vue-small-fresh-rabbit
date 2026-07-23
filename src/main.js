93174130058b53200738c1bfbbe3e4fded577739
lyxin <3077250484@qq.com>
2026-07-23 16:22:14 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -11,11 +12,15 @@ import '@/styles/common.scss'
 //引入懒加载指令插件并且注册
 import { lazyPlugin } from './directives/index.js' 
 
+//引入全局组件插件
+import {componentPlugin} from '@/components'
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
-
-
-app.mount('#app')
+app.use(componentPlugin)
+app.mount('#app')   
