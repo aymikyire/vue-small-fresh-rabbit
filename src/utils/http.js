61130e6ca61fa6a372f2adcd2139c1cc508b6d75
lyxin <3077250484@qq.com>
2026-07-23 17:15:36 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,10 +2,11 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/user'
 
 const httpInstance = axios.create({
-    baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout:10000
+    baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 10000
 
 })
 
@@ -13,6 +14,13 @@ const httpInstance = axios.create({
 
 //axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    //1.从Pinia获取token数据
+    const userStore = useUserStore()
+    //2.按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
@@ -20,8 +28,8 @@ httpInstance.interceptors.request.use(config => {
 httpInstance.interceptors.response.use(res => res.data, e => {
     //统一错误提示
     ElMessage({
-        type:'warning',
-        message:e.response.data.message
+        type: 'warning',
+        message: e.response.data.message
     })
     return Promise.reject(e)
 })
